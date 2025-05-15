ea0530a45d7394e5faa61d8790e18aca0ae28500
FelopaterAdel <felopateradel@yahoo.com>
2025-05-16 00:11:37 +0300

    Product Controller

--- controllers/product.js
+++ controllers/product.js
@@ -1,10 +1,35 @@
 import {catchAsync} from "../Utlites/wrapperFunction.js";
 // imprt mongose schemas here
-
+import productModel from "../models/product.js"
 export const getAllProducts = catchAsync(async (req, res, next) => {   
-    // Logic to get all products
-})
+let product = await productModel.find()
+    res.status(200).json({
+        status: "success",
+        data: product,
+    });})
 
 export const getOneProduct = catchAsync(async (req, res, next) => {   
-    // Logic to get all products
-})
+ const { id } = req.params;
+    let product = await productModel.findOne({ _id: id });
+
+    if (product) {
+        res.status(200).json({
+            status: "success",
+            data: product,
+            
+        });
+    } else {
+        next(AppError(404,"product not found"))
+       
+    }})
+export const createProduct = catchAsync(async (req, res, next) => {   
+let newProduct= req.body;
+
+    const product = await productModel.create(newProduct);
+
+    res.status(201).json({
+        status: "success",
+        message: "saved successfully",
+        data: product,
+    
+    });})
--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {getAllProducts, getOneProduct} from '../controllers/product.js';
+import {getAllProducts, getOneProduct,createProduct} from '../controllers/product.js';
 const router = express.Router();
 
 router.get('/', getAllProducts);
 router.get('/:id', getOneProduct);
+router.post('/', createProduct);
 
 export default router;
